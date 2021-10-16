f84b18ce17a77ce34b6a29ad8c9867414820e614
siva-pulavarthy <sivakumarsrisaipulavarthy@my.unt.edu>
2021-10-15 22:28:11 -0500

    siva

---
+++ Source Code/TheBowlDB.sql
@@ -0,0 +1,96 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE TABLE `tbl_admin` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Creating Basic Admin User
+
+INSERT INTO `tbl_admin` (`id`, `full_name`, `username`, `password`) VALUES
+(1, 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- Table for Food Category
+
+CREATE TABLE `tbl_category` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `featured` varchar(10) NOT NULL,
+  `active` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Table for Food Items Display
+
+CREATE TABLE `tbl_food` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `featured` varchar(10) NOT NULL,
+  `active` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Table for Food Order Placement
+
+CREATE TABLE `tbl_order` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `food` varchar(150) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `order_date` datetime NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `customer_name` varchar(150) NOT NULL,
+  `customer_contact` varchar(20) NOT NULL,
+  `customer_email` varchar(150) NOT NULL,
+  `customer_address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- Primary Key Assignment for all the tables 
+
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `tbl_food`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`id`);
+
+-- Adding Auto Increment Feature for the Tables
+
+ALTER TABLE `tbl_admin`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+
+ALTER TABLE `tbl_category`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+
+ALTER TABLE `tbl_food`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+
+ALTER TABLE `tbl_order`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+COMMIT;
+
+
