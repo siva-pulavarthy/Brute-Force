19f5e863e91844a245cb5273273de82e9c0aec12
akhil kata <30366367+akhilkata@users.noreply.github.com>
2021-10-29 21:45:04 -0500

    Add files via upload

---
+++ Phase-2/partials-front/footer.php
@@ -0,0 +1,24 @@
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="footer">
+        <div class="container text-center">
+            <p>2021 All rights reserved. The Bowl <a href="#">Brute Force</a></p>
+        </div>
+    </section>
+
+</body>
+</html>
