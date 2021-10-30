28e39b7b825db53a4edc8da59b74af66a956db89
akhil kata <30366367+akhilkata@users.noreply.github.com>
2021-10-29 21:43:29 -0500

    Add files via upload

---
+++ Phase-2/admin/partials/footer.php
@@ -0,0 +1,10 @@
+
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 All rights reserved, The Bowl. Developed By - <a href="#">Brute Force</a></p>
+            </div>
+        </div>
+       
+
+    </body>
+</html>
