50c3022c75b8b0dc7b22fb0a245474bb74622ccb
siva-pulavarthy <90059593+siva-pulavarthy@users.noreply.github.com>
2021-11-19 23:24:47 -0600

    Add files via upload

---
+++ Final-source-code/admin/assetsForSideBar/js/main.js
@@ -0,0 +1,23 @@
+
+const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId)
+
+
+    if(toggle && nav && bodypd && headerpd){
+        toggle.addEventListener('click', ()=>{
+            
+            nav.classList.toggle('showa')
+           
+            toggle.classList.toggle('bx-x')
+            
+            bodypd.classList.toggle('body-pd')
+            
+            headerpd.classList.toggle('body-pd')
+        })
+    }
+}
+
+showNavbar('header-toggle','nav-bar','body-pd','header')
